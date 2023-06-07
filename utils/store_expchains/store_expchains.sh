717f87c4ff98e0f11e9a0545a9f8d86d4171935c
Igor Matsko <imatsko@tradingview.com>
2023-06-07 10:50:16 +0300

    IDC-1404: fix

--- utils/store_expchains/store_expchains.sh
+++ utils/store_expchains/store_expchains.sh
@@ -6,21 +6,24 @@ IDC_HOST="$1"
 IDC_TVC_HOST="$2"
 EXPCHAINS_BRANCH="$3"
 
-SI_GROUPS=$(curl "$IDC_HOST/meta/info.json" | jq -r  '.groups | keys | .[]' | grep '_futures$')
 
 SYMBOLINFO_DIR="$DIR/symbolinfo"
 mkdir -p "$SYMBOLINFO_DIR"
+echo "Getting groups from $IDC_HOST"
+SI_GROUPS=$(curl -s "$IDC_HOST/meta/info.json" | jq -r  '.groups | keys | .[]' | grep '_futures$')
 while read -r SI_GROUP; do
     echo "Getting $SI_GROUP"
     curl -s "$IDC_HOST/symbol_info?group=$SI_GROUP" > "$SYMBOLINFO_DIR/$SI_GROUP.json"
 done <<< "$SI_GROUPS"
 
-SI_TVC_GROUPS="$(curl "$IDC_TVC_HOST/meta/info.json" | jq -r  '.groups | keys | .[]' | grep '_futures$')"
+echo "Getting groups from $IDC_TVC_HOST"
+SI_TVC_GROUPS="$(curl -s "$IDC_TVC_HOST/meta/info.json" | jq -r  '.groups | keys | .[]' | grep '_futures$')"
 while read -r SI_TVC_GROUP; do
     echo "Getting $SI_TVC_GROUP"
     curl -s "$IDC_TVC_HOST/symbol_info?group=$SI_TVC_GROUP" > "$SYMBOLINFO_DIR/$SI_TVC_GROUP.json"
 done <<< "$SI_TVC_GROUPS"
 
+echo "Getting manual groups"
 SI_OTHER_GROUPS=(moex_iss_futures nymex_cfd_futures_internal)
 for GROUP in "${SI_OTHER_GROUPS[@]}"; do
     echo "Getting $GROUP"
