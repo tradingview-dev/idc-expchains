e1482d889d191d9d1cf1a95c7f01518b577f0705
nmishenin <nmishenin@tradingview.com>
2024-02-01 11:46:52 +0000

    IDC-1528: test

--- utils/biva_data/update_biva_data.sh
+++ utils/biva_data/update_biva_data.sh
@@ -11,25 +11,20 @@ if [ "$1" == "" ]; then
 	exit -1
 fi
 
-EXPCHAINS_BRANCH="$1"
-
-if [ "$EXPCHAINS_BRANCH" == "staging" ]; then
-	echo "Upload files to staging"
-else
-	echo "WARNING: Files will be uploaded to production storage"
-fi
+STAGING_BRANCH="idc-1528-add-parser-for-isins-and-descriptions-biva"
+MASTER_BRANCH="staging"
 
 EXP_CHAINS_DIR="./idc-expchains"
 
 if [ ! -d "$EXP_CHAINS_DIR" ]; then
-    echo "Clone branch ${EXPCHAINS_BRANCH} from repo ${EXPCHAINS_REPO}"
-    git clone --depth 1 --single-branch -b $EXPCHAINS_BRANCH "$EXPCHAINS_REPO" "$EXP_CHAINS_DIR"
+    echo "Clone branch ${STAGING_BRANCH} from repo ${EXPCHAINS_REPO}"
+    git clone --depth 1 --single-branch -b $STAGING_BRANCH "$EXPCHAINS_REPO" "$EXP_CHAINS_DIR"
 else
     pushd "$EXP_CHAINS_DIR"
-    echo "Update branch ${EXPCHAINS_BRANCH} from repo ${EXPCHAINS_REPO}"
+    echo "Update branch ${STAGING_BRANCH} from repo ${EXPCHAINS_REPO}"
     git fetch
-    git checkout $EXPCHAINS_BRANCH
-    git pull origin $EXPCHAINS_BRANCH
+    git checkout $STAGING_BRANCH
+    git pull origin $STAGING_BRANCH
     popd
 fi
 
@@ -51,11 +46,22 @@ mv "${FILE1}" "${EXP_CHAINS_DIR}/dictionaries/"
 pushd "$EXP_CHAINS_DIR"
 git add "dictionaries/biva_data.csv"
 if [ "$(git status -s)" = "" ]; then
-    echo "No changes in $EXPCHAINS_BRANCH"
+    echo "No changes in $STAGING_BRANCH"
 else
-    echo "Update expchains in $EXPCHAINS_BRANCH"
+    echo "Update expchains in $STAGING_BRANCH"
     git --no-pager -c color.ui=always diff --staged
     git commit -m "Autocommit biva data"
-    git push origin "$EXPCHAINS_BRANCH"
+    git push origin "$STAGING_BRANCH"
 fi
+git checkout $MASTER_BRANCH
+git pull origin $MASTER_BRANCH
+cp "${EXP_CHAINS_DIR}/dictionaries/biva_data.csv" .
+git add "biva_data.csv"
+if [ "$(git status -s)" = "" ]; then
+    echo "No changes in $MASTER_BRANCH"
+else
+    echo "Update expchains in $MASTER_BRANCH"
+    git --no-pager -c color.ui=always diff --staged
+    git commit -m "Autocommit biva data to master"
+    git push origin "$MASTER_BRANCH"
 popd
