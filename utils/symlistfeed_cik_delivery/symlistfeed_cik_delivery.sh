0a5324a061bb422e3e02551c831c029af6476f51
dkazeev <dkazeev@tradingview.com>
2024-08-27 15:36:20 +0300

    IDC-3362: add symlisteed cik delivery

---
+++ utils/symlistfeed_cik_delivery/symlistfeed_cik_delivery.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+
+EXPCHAINS_REPO="git@git.xtools.tv:idc/idc-expchains.git"
+
+if [ "$1" == "" ]; then
+	echo "Please specify expchains branch"
+	exit -1
+fi
+
+EXPCHAINS_BRANCH="$1"
+
+if [ "$EXPCHAINS_BRANCH" == "staging" ]; then
+  SYMLIST_ENVIRONMENT = "staging"
+	echo "Upload files to staging"
+else
+  SYMLIST_ENVIRONMENT = "master"
+	echo "WARNING: Files will be uploaded to production storage"
+fi
+
+EXP_CHAINS_DIR="./idc-expchains"
+
+if [ ! -d "$EXP_CHAINS_DIR" ]; then
+    echo "Clone branch ${EXPCHAINS_BRANCH} from repo ${EXPCHAINS_REPO}"
+    git clone --depth 1 --single-branch -b $EXPCHAINS_BRANCH "$EXPCHAINS_REPO" "$EXP_CHAINS_DIR"
+else
+    pushd "$EXP_CHAINS_DIR"
+    echo "Update branch ${EXPCHAINS_BRANCH} from repo ${EXPCHAINS_REPO}"
+    git fetch
+    git checkout $EXPCHAINS_BRANCH
+    git pull origin $EXPCHAINS_BRANCH
+    popd
+fi
+
+python3 "$SCRIPTPATH/symlisteed_cik_delivery.py" --provider "dataops" --filename "cik_codes.json" --ruleset-filename "cik_codes_v1.json" --environment "$SYMLIST_ENVIRONMENT"
