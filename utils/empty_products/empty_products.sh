25264ce70d1ab9ba0e846941181ff11869ffbe6b
dkazeev <dkazeev@tradingview.com>
2024-09-04 20:16:16 +0300

    IDC-3239: empty products script

---
+++ utils/empty_products/empty_products.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+
+EXPCHAINS_REPO="git@git.xtools.tv:idc/idc-expchains.git"
+
+if [ "$1" == "" ]; then
+	echo "Please specify expchains branch"
+	exit -1
+fi
+
+EXPCHAINS_BRANCH="$1"
+
+if [ "$EXPCHAINS_BRANCH" == "staging" ]; then
+	echo "Upload files to staging"
+else
+	echo "WARNING: Files will be uploaded to production storage"
+fi
+
+EXP_CHAINS_DIR="./idc-expchains"
+
+if [ ! -d "$EXP_CHAINS_DIR" ]; then
+    echo "Clone branch ${EXPCHAINS_BRANCH} from repo ${EXPCHAINS_REPO}"
+    git clone --depth 1 --single-branch -b $EXPCHAINS_BRANCH "$EXPCHAINS_REPO" "$EXP_CHAINS_DIR"
+else
+    pushd "$EXP_CHAINS_DIR"
+    echo "Update branch ${EXPCHAINS_BRANCH} from repo ${EXPCHAINS_REPO}"
+    git fetch
+    git checkout $EXPCHAINS_BRANCH
+    git pull origin $EXPCHAINS_BRANCH
+    popd
+fi
+
+
+python3 "$SCRIPTPATH/empty_products.py"
+
+FILE1="${SCRIPTPATH}/empty_products"
+
+if [ ! -f "$FILE1" ]; then
+    echo "All products filled!"
+    exit 0
+fi
+
+
+CHANNEL="#empty-products"
+NAME="Jenkins-IDC"
+TEXT=`cat $FILE1`
+
+curl -X POST \
+	--data-urlencode "payload={\"channel\": \"$CHANNEL\", \"username\": \"$NAME\", \"text\": \"$TEXT\", \"link_names\": \"1\" }" \
+    "$SLACK_NOTIFY_URL"
+
+rm "$FILE1"
