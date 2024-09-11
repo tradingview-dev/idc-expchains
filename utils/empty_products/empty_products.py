9f14d1c7561f3a9906635b2553997eb3be3b8ace
dkazeev <dkazeev@tradingview.com>
2024-09-11 18:23:55 +0300

    IDC-3239: empty products script(2)

--- utils/empty_products/empty_products.py
+++ utils/empty_products/empty_products.py
@@ -5,9 +5,22 @@ symbols = pd.read_json("https://s3.amazonaws.com/tradingview-symbology-staging/s
 
 symbols = symbols.query('`symbol-type`== "commodity" or `symbol-type`=="futures"')
 
-symbols = symbols[["symbol", "group", "product", "description"]]
+symbols = symbols[["symbol", "group", "product", "description", "provider-id"]]
 symbols = symbols[symbols['product'].isnull() | (symbols['product'] == "")]
-symbols = symbols[["symbol", "group", "description"]]
+empty_products = symbols[["symbol", "group", "description"]]
 
-if not symbols.empty:
-    symbols.to_csv("empty_products", index=False)
+
+if not empty_products.empty:
+    empty_products.to_csv("empty_products", index=False)
+
+idc_symbols = symbols[symbols["provider-id"].isin(["ice", "tvc", "six", "moex"])]
+not_idc_symbols = symbols[~symbols["provider-id"].isin(["ice", "tvc", "six", "moex"])]
+
+empty_products_idc = idc_symbols[["symbol", "group", "description"]]
+empty_products_not_idc = not_idc_symbols[["symbol", "group", "description"]]
+
+if not empty_products_idc.empty:
+    empty_products_idc.to_csv("empty_products_idc", index=False)
+
+if not empty_products_not_idc.empty:
+    empty_products_not_idc.to_csv("empty_products_not_idc", index=False)
