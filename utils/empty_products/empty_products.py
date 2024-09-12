fe408a241c3dc75a1591e9015d912200aaa647e0
dkazeev <dkazeev@tradingview.com>
2024-09-12 20:24:16 +0300

    IDC-3239: empty products script(5)

--- utils/empty_products/empty_products.py
+++ utils/empty_products/empty_products.py
@@ -1,3 +1,5 @@
+import json
+
 import pandas as pd
 
 symbols = pd.read_json("https://s3.amazonaws.com/tradingview-symbology-staging/symbols.json")
@@ -20,7 +22,19 @@ empty_products_idc = idc_symbols[["symbol", "group", "description"]]
 empty_products_not_idc = not_idc_symbols[["symbol", "group", "description"]]
 
 if not empty_products_idc.empty:
-    empty_products_idc.to_csv("empty_products_idc", index=False)
+    msg_header = "@idc-dutyman\nEMPTY PRODUCTS LIST\n"
+    msg_body = empty_products_idc.to_csv(index=False)
+    output_json = {"blocks": [{"type": "section", "text": {"type": "mrkdwn", "text": msg_header}},
+                              {"type": "section", "text": {"type": "mrkdwn", "text": msg_body}}]}
+    with open('empty_products_idc', 'w', encoding='utf-8') as f:
+        json.dump(output_json, f, ensure_ascii=False, indent=4)
+    #empty_products_idc.to_csv("empty_products_idc", index=False)
 
 if not empty_products_not_idc.empty:
-    empty_products_not_idc.to_csv("empty_products_not_idc", index=False)
+    msg_header = "@feed-qa @versus-data\nEMPTY PRODUCTS LIST\n"
+    msg_body = empty_products_not_idc.to_csv(index=False)
+    output_json = {"blocks": [{"type": "section", "text": {"type": "mrkdwn", "text": msg_header}},
+                              {"type": "section", "text": {"type": "mrkdwn", "text": msg_body}}]}
+    with open('empty_products_not_idc', 'w', encoding='utf-8') as f:
+        json.dump(output_json, f, ensure_ascii=False, indent=4)
+    #empty_products_not_idc.to_csv("empty_products_not_idc", index=False)
