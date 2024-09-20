60fcdf347dfe1c997fd57ce52a1430b6ed9e40c3
nmishenin <nmishenin@tradingview.com>
2024-09-20 10:22:08 +0000

    IDC-3555: lang and swartz parser

---
+++ utils/lang_and_shwarz_data/update_lang_and_shwarz_data.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -e
+
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+
+EXPCHAINS_REPO="git@git.xtools.tv:idc/idc-expchains.git"
+
+if [ "$1" == "" ]; then
+	echo "Please specify expchains branch"
+	exit -1
+fi
+
+EXPCHAINS_BRANCH="$1"
+EXCHANGE="$2"
+
+if [ "$EXPCHAINS_BRANCH" == "staging" ]; then
+	echo "Upload files to staging"
+else
+	echo "WARNING: Files will be uploaded to production storage"
+fi
+
+EXP_CHAINS_DIR="./idc-expchains"
+
+if [ ! -d "$EXP_CHAINS_DIR" ]; then
+    echo "Clone branch ${EXPCHAINS_BRANCH} from repo ${EXPCHAINS_REPO}"
+    git clone --depth 1 --single-branch -b $EXPCHAINS_BRANCH "$EXPCHAINS_REPO" "$EXP_CHAINS_DIR"
+else
+    pushd "$EXP_CHAINS_DIR"
+    echo "Update branch ${EXPCHAINS_BRANCH} from repo ${EXPCHAINS_REPO}"
+    git fetch
+    git checkout $EXPCHAINS_BRANCH
+    git pull origin $EXPCHAINS_BRANCH
+    popd
+fi
+
+mkdir -p "${EXP_CHAINS_DIR}/dictionaries/"
+
+python3 "$SCRIPTPATH/lang_and_shwarz_data.py" -exchange "$EXCHANGE"
+
+if [[ "$EXCHANGE" == "x" ]]; then
+  FILE="LSX.csv"
+else
+  FILE="LS.csv"
+fi
+
+FILE_PATH="${SCRIPTPATH}/${FILE}"
+
+FILE_SIZE=$(stat --printf '%s' "${FILE}")
+
+if [ "$FILE_SIZE" -lt "1000" ]; then
+    echo "ERROR: One or both resulting files are too small"
+    exit 1
+fi
+
+mv "${FILE_PATH}" "${EXP_CHAINS_DIR}/dictionaries/"
+
+pushd "$EXP_CHAINS_DIR"
+git add "dictionaries/${FILE}"
+if [ "$(git status -s)" = "" ]; then
+    echo "No changes in $EXPCHAINS_BRANCH"
+else
+    echo "Update expchains in $EXPCHAINS_BRANCH"
+    git --no-pager -c color.ui=always diff --staged
+    git commit -m "Autocommit lang_and_shwarz_data data"
+    git push origin "$EXPCHAINS_BRANCH"
+fi
+popd
