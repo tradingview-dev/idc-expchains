5e245328fc0547930ba315c2f1ff073aac2cd845
dkazeev <dkazeev@tradingview.com>
2024-09-27 17:39:40 +0300

    IDC-3239: empty products script, post from python

--- utils/empty_products/empty_products.py
+++ utils/empty_products/empty_products.py
@@ -1,6 +1,38 @@
-import json
-
 import pandas as pd
+import json
+import requests
+import argparse
+
+def post_to_slack(product_list, attachment_header, hook):
+    msg_attachments = []
+    empty_products_list = product_list.apply(lambda row: ';'.join(row.values.astype(str)), axis=1).tolist()
+    empty_products_list.insert("symbol;group;description", 0)
+    error_list = [f"- {p}" for p in empty_products_list]
+    if len(error_list) > 25:
+        error_list = ["..."] + error_list[-24:]
+    attachment_body = "\n".join(error_list)
+    msg_attachments.append({
+        "blocks": [
+            {"type": "section", "text": {"type": "mrkdwn", "text": attachment_header}},
+            {"type": "section", "text": {"type": "mrkdwn", "text": attachment_body}}
+        ]
+    })
+    msg_text = f":one or several symbols not have products>"
+    msg_body = [{"type": "section", "text": {"type": "mrkdwn", "text": msg_text}}]
+    msg = {
+        "blocks": msg_body,
+        "attachments": msg_attachments,
+        "link_names": "1"
+    }
+    response = requests.post(hook, json=msg)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('idc_hook', type=str,
+                    help='IDC slack hook')
+parser.add_argument('hub_hook', type=str,
+                    help='hub slack hook')
+args = parser.parse_args()
+
 
 symbols = pd.read_json("https://s3.amazonaws.com/tradingview-symbology-staging/symbols.json")
 
@@ -22,19 +54,7 @@ empty_products_idc = idc_symbols[["symbol", "group", "description"]]
 empty_products_not_idc = not_idc_symbols[["symbol", "group", "description"]]
 
 if not empty_products_idc.empty:
-    msg_header = "@idc-dutyman\nEMPTY PRODUCTS LIST\n"
-    msg_body = empty_products_idc.to_csv(index=False)
-    output_json = {"blocks": [{"type": "section", "text": {"type": "mrkdwn", "text": msg_header}},
-                              {"type": "section", "text": {"type": "mrkdwn", "text": msg_body}}]}
-    with open('empty_products_idc', 'w', encoding='utf-8') as f:
-        json.dump(output_json, f, ensure_ascii=False, indent=4)
-    #empty_products_idc.to_csv("empty_products_idc", index=False)
+    post_to_slack(empty_products_idc, "@idc-dutyman\nEMPTY PRODUCTS LIST\n", args.idc_hook)
 
 if not empty_products_not_idc.empty:
-    msg_header = "@feed-qa @versus-data\nEMPTY PRODUCTS LIST\n"
-    msg_body = empty_products_not_idc.to_csv(index=False)
-    output_json = {"blocks": [{"type": "section", "text": {"type": "mrkdwn", "text": msg_header}},
-                              {"type": "section", "text": {"type": "mrkdwn", "text": msg_body}}]}
-    with open('empty_products_not_idc', 'w', encoding='utf-8') as f:
-        json.dump(output_json, f, ensure_ascii=False, indent=4)
-    #empty_products_not_idc.to_csv("empty_products_not_idc", index=False)
+    post_to_slack(empty_products_not_idc, "@feed-qa @versus-data\nEMPTY PRODUCTS LIST\n", args.hub_hook)
