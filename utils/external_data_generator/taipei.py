7d65b1edb383c2dae99d1c8a20064ed88a456370
imatsko <imatsko@tradingview.com>
2025-02-11 14:35:35 +0300

    IDC-4041: fix taipei data

--- utils/external_data_generator/taipei.py
+++ utils/external_data_generator/taipei.py
@@ -1,9 +1,8 @@
-import json
-
 import requests
 from deepdiff.serialization import json_dumps
 
-from utils import get_headers, request_with_retries, file_writer  # Importing functions from utils
+from utils import request_with_retries, file_writer
+
 
 def collect_info(url, additional_data=None):
     """
@@ -14,6 +13,7 @@ def collect_info(url, additional_data=None):
     """
     response = request_with_retries(url, post_data=additional_data)
     return response
+
 
 def join_objects(left, right, description_type):
     """
@@ -32,43 +32,42 @@ def join_objects(left, right, description_type):
     return [{'symbol': symbol, description_type: result_map[symbol]} for symbol in
             list(left_map.keys()) + [key for key in right_map.keys() if key not in left_map]]
 
+
 def taipei_handler():
     # ---descriptions------
     descriptions = []
 
     # Collect descriptions for ETF (English)
-    response = collect_info("https://info.tpex.org.tw/api/etfFilter?lang=en-us", {})
+    request = ("https://info.tpex.org.tw/api/etfFilter?lang=en-us", {})
+    print(f"Requesting {request}")
+    response = collect_info(request[0], request[1])
     if response != requests.Response():
         tmp = response.json().get('data', {})
         result = [{"symbol": item["stockNo"], "description": item["stockName"]} for item in tmp]
         descriptions = join_objects(descriptions, result, "description")
 
     # Collect domestic, foreign, and other descriptions in English
-    etn_urls = [
-        "https://www.tpex.org.tw/www/en-us/ETN/list",
-        "https://www.tpex.org.tw/www/en-us/ETN/list",
-        "https://www.tpex.org.tw/www/en-us/fund/recomSearch"
+    req_list = [
+        ("https://www.tpex.org.tw/www/en-us/ETN/list", {"type": "domestic", "response": "json"}),
+        ("https://www.tpex.org.tw/www/en-us/ETN/list", {"type": "foreign", "response": "json"}),
     ]
-
-    for url in etn_urls:
-        add_data = {
-            'type': 'domestic' if "ETN" in url else 'foreign',
-            'id': '',
-            'response': 'json'
-        }
-        response = collect_info(url, add_data)
+    for request in req_list:
+        print(f"Requesting {request}")
+        response = collect_info(request[0], request[1])
         if response != requests.Response():
             response_data = response.json()
             if 'tables' in response_data and len(response_data['tables']) > 0:
                 result = [{'symbol': item[0], 'description': item[1]} for item in response_data['tables'][0]['data']]
                 descriptions = join_objects(descriptions, result, "description")
 
-    file_writer(json_dumps(descriptions, indent=4, ensure_ascii=False), 'taipei_descriptions.json')
+    file_writer(json_dumps(sorted(descriptions, key=lambda r: r["symbol"]), indent=4, ensure_ascii=False), 'taipei_descriptions.json')
 
     # ---local-descriptions------
     local_descriptions = []
 
     # Collect local descriptions for ETF (Chinese)
+    request = ("https://info.tpex.org.tw/api/etfFilter?lang=zh-tw", {})
+    print(f"Requesting {request}")
     response = collect_info("https://info.tpex.org.tw/api/etfFilter?lang=zh-tw", {})
     if response != requests.Response():
         tmp = response.json().get('data', {})
@@ -76,24 +75,21 @@ def taipei_handler():
         local_descriptions = join_objects(local_descriptions, result, "local-description")
 
     # Collect local descriptions (domestic, foreign, etc.) in Chinese
-    local_etn_urls = [
-        "https://www.tpex.org.tw/www/zh-tw/ETN/list",
-        "https://www.tpex.org.tw/www/zh-tw/ETN/list",
-        "https://www.tpex.org.tw/www/zh-tw/company/otcSearch",
-        "https://www.tpex.org.tw/www/zh-tw/company/emergingSearch",
-        "https://www.tpex.org.tw/www/zh-tw/company/emergingSearch"
+    req_list = [
+        ("https://www.tpex.org.tw/www/zh-tw/company/otcSearch", {"cate": "02", "type": "stkType", "stkType": "+", "response": "json"}),
+        ("https://www.tpex.org.tw/www/zh-tw/company/otcSearch", {"cate": "02", "type": "stkType", "stkType": "RR", "response": "json"}),
+        ("https://www.tpex.org.tw/www/zh-tw/ETN/list", {"type": "domestic", "response": "json"}),
+        ("https://www.tpex.org.tw/www/zh-tw/ETN/list", {"type": "foreign", "response": "json"}),
+        ("https://www.tpex.org.tw/www/zh-tw/company/emergingSearch", {"type": "domestic", "response": "json"}),
+        ("https://www.tpex.org.tw/www/zh-tw/company/emergingSearch", {"type": "foreign", "response": "json"}),
     ]
-
-    for url in local_etn_urls:
-        add_data = {
-            'type': 'domestic' if "ETN" in url else 'foreign',
-            'response': 'json'
-        }
-        response = collect_info(url, add_data)
+    for request in req_list:
+        print(f"Requesting {request}")
+        response = collect_info(request[0], request[1])
         if response != requests.Response():
             response_data = response.json()
             if 'tables' in response_data and len(response_data['tables']) > 0:
                 result = [{'symbol': item[0], 'local-description': item[1]} for item in response_data['tables'][0]['data']]
                 local_descriptions = join_objects(local_descriptions, result, "local-description")
 
-    file_writer(json_dumps(local_descriptions, indent=4, ensure_ascii=False), 'taipei_local_descriptions.json')
+    file_writer(json_dumps(sorted(local_descriptions, key=lambda r: r["symbol"]), indent=4, ensure_ascii=False), 'taipei_local_descriptions.json')
