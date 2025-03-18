eaec2d00c728dfc8ad7146fc3a15f110bea336fa
dkazeev <dkazeev@tradingview.com>
2025-03-18 19:12:05 +0300

    IDC-1840: fix cftc-codes

--- utils/external_data_generator/cftc_code.py
+++ utils/external_data_generator/cftc_code.py
@@ -12,9 +12,29 @@ def cftc_handler():
 
     excel_data = pd.read_excel(xlsx_filename)
 
-    csv_filename = "strike-price-report.csv"
-    excel_data.to_csv(csv_filename, index=False)
+    # Define a dictionary to map Exchange IDs to exchange names
+    exchange_mapping = {
+        '01': 'CBOT',
+        '02': 'CME',
+        '06': 'ICEUS',
+        '07': 'COMEX',
+        '12': 'NYMEX',
+        'E': 'CBOE',
+        'SG': 'SGX',
+        '41': 'ICEEU'
+    }
+
+    exchange_groups = excel_data.groupby('Exchange ID')
+
+    for exchange_id, group in exchange_groups:
+        if str(exchange_id) in exchange_mapping:
+            exchange_name = exchange_mapping.get(str(exchange_id), str(exchange_id))
+
+            output_filename = f"cftc_{exchange_name}.csv"
+            group.to_csv(output_filename, index=False)
+            print(f"CSV file for Exchange ID {exchange_id} ({exchange_name}) has been saved as {output_filename}")
+        else:
+            print(f"Exchange ID {exchange_id} is not in the mapping, skipping file creation.")
+
 
     os.remove(xlsx_filename)
-
-    print(f"CSV file has been saved as {csv_filename} ")
