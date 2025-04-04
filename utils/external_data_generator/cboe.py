3b9abaad17fd10648930109059549539d8e9c415
nmishenin <nmishenin@tradingview.com>
2025-04-04 17:37:58 +0000

    Revert "OPRA: change CBOE state for opra options"
    This reverts commit 33b705c19e2715e4d6cb29dead6f7349a1132b57.

--- utils/external_data_generator/cboe.py
+++ utils/external_data_generator/cboe.py
@@ -5,9 +5,5 @@ def cboe_handler():
     csv = requests.get("https://www.cboe.com/us/equities/market_statistics/listed_symbols/csv/").text
     
     with open("cboe.csv", "w") as file:
-        file.write("Name;underlying-symbol\n")
         for line in csv.split("\n"):
-            symbol = line.split(',')[0]
-            if symbol == "Name":
-                continue
-            file.write(f"{line.split(',')[0]};CBOE:{line.split(',')[0]}\n")
+            file.write(f"{line.split(',')[0]}\n")
