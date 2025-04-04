33b705c19e2715e4d6cb29dead6f7349a1132b57
nmishenin <nmishenin@tradingview.com>
2025-04-04 17:13:15 +0000

    OPRA: change CBOE state for opra options

--- utils/external_data_generator/cboe.py
+++ utils/external_data_generator/cboe.py
@@ -5,5 +5,9 @@ def cboe_handler():
     csv = requests.get("https://www.cboe.com/us/equities/market_statistics/listed_symbols/csv/").text
     
     with open("cboe.csv", "w") as file:
+        file.write("Name;underlying-symbol\n")
         for line in csv.split("\n"):
-            file.write(f"{line.split(',')[0]}\n")
+            symbol = line.split(',')[0]
+            if symbol == "Name":
+                continue
+            file.write(f"{line.split(',')[0]};CBOE:{line.split(',')[0]}\n")
