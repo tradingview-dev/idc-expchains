969ee770ab28f8236a5050b75f7d3814abf8668f
nmishenin <nmishenin@tradingview.com>
2025-04-29 13:44:05 +0000

    IDC-4353: fix aquis filter file

--- utils/external_data_generator/aquis.py
+++ utils/external_data_generator/aquis.py
@@ -1,26 +1,52 @@
 from bs4 import BeautifulSoup
+from typing import List
 
 import openpyxl
 import os
 import urllib.request
 import requests
 
-def get_url():
+BASE_URI = "https://www.aquis.eu"
 
-    res = dict()
+def get_aquis_symbols()-> List[List[str]]:
+    """
+    request symbols
+    :return slice with symbols
+    """
+    res = []
+
+    companies = requests.get(f"{BASE_URI}/companies").content
+    soup = BeautifulSoup(companies, "html.parser")
+    symbols = soup.find_all("a", class_="chakra-link css-pz0aee")
+    for symbol in symbols:
+        res.append(get_symbol_params(symbol.text))
+    return res
+
+def get_symbol_params(symbol: str)-> List[str]:
+    """
+    :param symbol: symbol string
+    :return: array with symbols params
+    """
+
+    symbol_page = requests.get(f"{BASE_URI}/companies/{symbol}").content
+
+    soup = BeautifulSoup(symbol_page, "html.parser")
+    isin = soup.find("p", class_="chakra-text css-4vttjp").text
+    description = soup.find("h1", class_="chakra-heading css-135d5ex").text
+    currency = soup.find("p", class_="chakra-text css-bjhtoj").text.split(" ")[2]
+    return [description, symbol, isin, currency]
+
+
+def get_secondary_listing_url_file()-> str:
 
     html = requests.get("https://www.aquis.eu/stock-exchange/statistics").content
 
     soup = BeautifulSoup(html, "html.parser")
     href = soup.find_all("a", class_="chakra-link css-fhzrj1")
 
-    primary = href[0]
     secondary = href[1]
 
-    res['primary'] = primary['href']
-    res['secondary'] =  secondary['href']
-
-    return res
+    return secondary['href']
 
 
 def download_file(url: str, destination: str) -> None:
@@ -38,35 +64,25 @@ def xlsx_to_csv(csv_res: str, destination: str) -> None:
     """
     workbook = openpyxl.load_workbook(destination, read_only=True)
 
-    with open(csv_res, 'a', newline='', encoding='utf-8') as csv_file:
+    with open(csv_res, 'w', newline='', encoding='utf-8') as csv_file:
 
-        if destination.split(".")[0] == "primary":
+        sheet = workbook.active
 
-            sheet = workbook["Trading Data"]
+        for row in sheet.iter_rows(values_only=True):
 
-            for row in sheet.iter_rows(values_only=True):
+            if row[0] is not None and row[1] is not None and row[2] is not None and row[3] is not None:
+                csv_file.write(f"{row[0]};{row[1]};{row[2]};{row[3]}\n")
 
-                if row[0] is not None and row[1] is not None and row[3] is not None and row[4] is not None:
-                    csv_file.write(f"{row[0]};{row[1]};{row[3]};{row[4]}\n")
-
-        else:
-
-            sheet = workbook.active
-
-            for row in sheet.iter_rows(values_only=True):
-
-                if row[0] is not None and row[1] is not None and row[2] is not None and row[3] is not None:
-                    csv_file.write(f"{row[0]};{row[1]};{row[2]};{row[3]}\n")
+        for symbol in get_aquis_symbols():
+            csv_file.write(f"{symbol[0]};{symbol[1]};{symbol[2]};{symbol[3]}\n")
 
 
 def aquis_handler():
 
-    listings = get_url()
+    secondary = get_secondary_listing_url_file()
 
-    for listing in listings:
+    download_file(secondary, f"aquis.xlsx")
 
-        download_file(listings[listing], f"{listing}.xlsx")
+    xlsx_to_csv(f"aquis.csv", f"aquis.xlsx")
 
-        xlsx_to_csv(f"aquis.csv", f"{listing}.xlsx")
-
-        os.remove(f"{listing}.xlsx")
+    os.remove(f"aquis.xlsx")
