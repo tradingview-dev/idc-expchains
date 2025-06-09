57b902f069b8fe66993ebb3cfe902f02e8e7da63
nmishenin <nmishenin@tradingview.com>
2025-06-09 17:10:56 +0000

    HOTFIX: fix nasdaq gids indices list

--- utils/external_data_generator/nasdaq_gids.py
+++ utils/external_data_generator/nasdaq_gids.py
@@ -9,8 +9,9 @@ DESTINATION = 'nasdaq_gids_symbols.xlsx'
 
 MY_CSV = "nasdaq_gids_symbols.csv"
 
-URL = 'https://indexes.nasdaqomx.com/Index/ExportDirectory'
+URLS = ['https://indexes.nasdaqomx.com/Index/ExportDirectory', 'https://indexes.nasdaqomx.com/Home/ExportGidsDirectory']
 
+gids_indices = set()
 
 def download_file(url: str, destination: str) -> None:
     """
@@ -33,15 +34,19 @@ def get_indices_from_xlsx(destination: str) -> list:
     :param destination: Path to the xlsx file
     :return: Sorted list of indices
     """
-    gids_indices = []
     try:
         workbook = openpyxl.load_workbook(destination, read_only=True)
         sheet = workbook["Index Directory"]
 
         for row in sheet.iter_rows(values_only=True):
-            if (row[7] == "" and row[6] == "SandP") or row[0] == "Symbol":
-                continue
-            gids_indices.append(row[0])
+
+            if len(row) == 3:
+                if row[2] != "I":
+                    continue
+            else:
+                if (row[7] == "" and row[6] == "SandP") or row[0] == "Symbol":
+                    continue
+            gids_indices.add(row[0])
 
         workbook.close()
     except Exception as e:
@@ -58,7 +63,7 @@ def write_to_csv(indices: list) -> None:
     :param indices: Sorted list of indices to save in the file
     """
     try:
-        with open(MY_CSV, 'w', newline='', encoding='utf-8') as csv_file:
+        with open(MY_CSV, 'a', newline='', encoding='utf-8') as csv_file:
             csv_file.write("tv-symbol\n")
 
             for index in indices:
@@ -71,8 +76,14 @@ def write_to_csv(indices: list) -> None:
 
 def nasdaq_gids_handler():
 
-    download_file(URL, DESTINATION)
+    for URL in URLS:
 
-    write_to_csv(get_indices_from_xlsx(DESTINATION))
+        download_file(URL, DESTINATION)
+
+        get_indices_from_xlsx(DESTINATION)
+
+    write_to_csv(gids_indices)
 
     os.remove(DESTINATION)
+
+nasdaq_gids_handler()
