adce8a56912e907fda5611b69abc57cf2397927f
imatsko <imatsko@tradingview.com>
2025-07-24 12:39:10 +0300

    IDC-4250: add script for external data parser to process otc

---
+++ utils/external_data_generator/otc.py
@@ -0,0 +1,109 @@
+import asyncio
+import json
+
+import aiohttp
+
+from DataGenerator import DataGenerator
+from utils import get_headers, load_from_repo
+
+
+class OtcDataGenerator(DataGenerator):
+
+    def __init__(self, branch):
+        super().__init__()
+        self._branch = branch
+
+    def __get_directory_url(self, page):
+        pageSize = 25
+        return f"https://backend.otcmarkets.com/otcapi/company-directory?page={page}&pageSize={pageSize}"
+
+    def __get_headers(self):
+        headers = get_headers()
+        headers.update({
+            "Origin": "https://www.otcmarkets.com",
+            "Referer": "https://www.otcmarkets.com/",
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache"
+        })
+        return headers
+
+    async def __request_all_records(self) -> list:
+        semaphore = asyncio.Semaphore(2)
+
+        async def limit_wrap(url, session):
+            async with semaphore:
+                return await self.__request_records(url, session)
+
+        async with aiohttp.ClientSession() as session:
+            records = []
+            self._logger.info("Requesting first page of OTC data")
+            pages, first_records = await self.__request_records(self.__get_directory_url(1), session)
+            self._logger.info(f"Data is on {pages} pages")
+            if pages is None:
+                pages = 1
+            records.extend([] if first_records is None else first_records)
+
+            urls = [self.__get_directory_url(page) for page in range(2, pages + 1)]
+
+            responses = [limit_wrap(url, session) for url in urls]
+            record_chunks = await asyncio.gather(*responses)
+            for _, chunk in record_chunks:
+                records.extend([] if chunk is None else chunk)
+            self._logger.info(f"Total loaded {len(records)} records")
+            return records
+
+    async def __request_records(self, url: str, session: aiohttp.ClientSession):
+        headers = self.__get_headers()
+        # self._logger.debug(f"{url} headers={headers}")
+        async with session.get(url, headers=headers) as response:
+            try:
+                # raw_data = await response.text()
+                # self._logger.debug(f"got response {response.status} {raw_data}")
+                data = await response.json()
+            except json.JSONDecodeError as e:
+                self._logger.error(f"Error decoding JSON for {url}: {e}")
+                return None, None
+
+            try:
+                self._logger.info(f"Loaded {len(data['records'])} records")
+                return data["pages"], data["records"]
+            except KeyError:
+                self._logger.info(f"No data {url}")
+                return None, None
+
+    def __merge(self, prev_records, new_records, field):
+        prev_dict = {r[field]: r for r in prev_records}
+        new_dict = {r[field]: r for r in new_records}
+
+        prev_dict.update(new_dict)
+        result = list(prev_dict.values())
+        result.sort(key=lambda r: r[field])
+        return result
+
+    def generate(self) -> list[str]:
+
+        records = asyncio.run(self.__request_all_records())
+        for r in records:
+            r.pop("joined", None)
+            r.pop("marketCap", None)
+
+        out_file = "otc_data.json"
+
+        load_from_repo([out_file], "staging")
+        with open(out_file, "r") as f:
+            prev_data = json.load(f)
+            self._logger.info(f"Previous data contains {len(prev_data)} records")
+        merged_data = self.__merge(prev_data, records, "symbol")
+        self._logger.info(f"Merged data contains {len(merged_data)} records")
+        with open(out_file, "w") as f:
+            json.dump(merged_data, f)
+
+        return [out_file]
+
+
+if __name__ == "__main__":
+    try:
+        OtcDataGenerator("staging").generate()
+        exit(0)
+    except OSError:
+        exit(1)
