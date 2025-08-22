0c740e8df56a1fa4ad3681ee7df3634bfa38201b
imatsko <imatsko@tradingview.com>
2025-08-22 16:29:37 +0300

    IDC-4686: fix

--- utils/external_data_generator/saudi.py
+++ utils/external_data_generator/saudi.py
@@ -21,7 +21,7 @@ class SAUDIDataGenerator(DataGenerator):
         file_writer(json_dumps(resp_json, indent=4, ensure_ascii=False), filename)
 
     def generate(self) -> list[str]:
-        url = 'https://www.saudiexchange.sa/wps/portal/saudiexchange/trading/participants-directory/issuer-directory/!ut/p/z1/lc_LDoIwEAXQb-EDzFzQ1rosCyqJPGqpYjeGhTEkWl0Yv190B75nN8m5k7nkqCbnm2u7by7tyTeHbt84vmWSI5oLFMpUEjoxVZzPkhApo3UfiExx6FzqIpoyqFVI7q88TMk6UGbjBZZQ4L_l8WYkvuddn0BbcSdJzAQiYDIELyoOLjx3eIAPT5qdp_PR2hptOpJBcAPgEs77/p0/IZ7_5A602H80OOMQC0604RU6VD10F2=CZ6_5A602H80OGSTA0QFSTBN9F10I5=NJgetCompanyListByMarknetAndSectors=/'
+        url = 'https://www.saudiexchange.sa/wps/portal/saudiexchange/trading/participants-directory/issuer-directory/!ut/p/z1/lc_LCsIwEAXQb-kHSG6jiXGZLhIL9hHTas1GspKAVhHx-63uWt-zGzh3mEscaYhr_TXs_CUcW7_v9o3jWyY56Fyg0LaSMMpWST5TMVJG1n0gMs1hcmkKOmXQq5i4v_KwJetAmY0XWEKD_5bHm5H4nnd9AlOLO1EJE6DAZAheVBxceO7wAB-etP5MToe6bhDSkYyiG8La9T0!/p0/IZ7_5A602H80OOMQC0604RU6VD10F2=CZ6_5A602H80OGSTA0QFSTBN9F10I5=NJgetCompanyListByMarknetAndSectors=/'
 
         saudi_main_market = "saudi_main_market.json"
         saudi_nomu_parallel_market = "saudi_nomu_parallel_market.json"
