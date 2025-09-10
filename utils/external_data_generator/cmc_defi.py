dc1c19f8fa119b7178d6bd8c34f913c9c0fc8f61
Artem Ilchenko <ailchenko@tradingview.com>
2025-09-10 21:31:05 +0300

    IDC-4723: Refactor and move defi_typespec.csv updater to external data updater

---
+++ utils/external_data_generator/cmc_defi.py
@@ -0,0 +1,93 @@
+import csv
+import json
+
+from DataGenerator import DataGenerator
+from lib.LoggableRequester import LoggableRequester
+from s3_utils import read_state
+from utils import get_headers, unpack_data
+
+
+class CMCDataGenerator(DataGenerator):
+
+    def __init__(self, branch, profile_name=None):
+        super().__init__()
+        self._branch = branch
+        self._profile_name = profile_name
+
+    def get_currencies(self, bucket_name):
+        compressed_data = read_state(bucket_name, "currencies.json", self._profile_name)
+        # unpack gzipped-data
+        content = unpack_data(compressed_data)
+        return json.loads(content)
+
+    def get_coinmarketcap_snapshot(self, cmc_id):
+        requester = LoggableRequester(self._logger, retries=5, delay=5)
+        try:
+            resp_1 = requester.request(LoggableRequester.Methods.GET,
+                                       f"https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?convert=BTC&start=1&sort=market_cap&limit=5000&CMC_PRO_API_KEY={cmc_id}",
+                                       get_headers())
+            resp_2 = requester.request(LoggableRequester.Methods.GET,
+                                       f"https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?convert=BTC&start=5001&sort=market_cap&limit=5000&CMC_PRO_API_KEY={cmc_id}",
+                                       get_headers())
+            return resp_1.json()['data'] + resp_2.json()['data']
+        except OSError as e:
+            self._logger.error(e)
+            raise e
+
+    def generate(self) -> list[str]:
+        cmc_ids = {
+            "staging": "98e9877a-f3b7-4945-94f6-9f155fec4a65",
+            "stable": "d2daa136-eb61-420b-8dcd-d412928f2f92",
+            "prod": "d2daa136-eb61-420b-8dcd-d412928f2f92"
+        }
+        currencies_buckets = {
+            "staging": "tradingview-currencies-staging",
+            "stable": "tradingview-currencies",
+            "prod": "tradingview-currencies"
+        }
+        coinmarketcap_snapshot = self.get_coinmarketcap_snapshot(cmc_ids[self._branch])
+        currencies = self.get_currencies(currencies_buckets[self._branch])
+        defi_typespec = "defi_typespec.csv"
+        self.run(coinmarketcap_snapshot, currencies, defi_typespec)
+        return [defi_typespec]
+
+    @staticmethod
+    def map_currency_id(currency: str) -> str:
+        if currency.upper() == "GRT":
+            return "XTVCGRAPH"
+        return f"XTVC{currency.upper()}"
+
+    @staticmethod
+    def read_currency_ids(currencies) -> dict:
+        result = {}
+        for record in currencies:
+            result[record["id"]] = record
+        return result
+
+    def run(self, coinmarketcap_snapshot, currencies, out_file) -> None:
+        defi_coins = [r for r in coinmarketcap_snapshot if "defi" in r.get("tags", [])]
+
+        currency_ids = self.read_currency_ids(currencies)
+
+        defi_coins_ids = []
+        for record in defi_coins:
+            cid = self.map_currency_id(record["symbol"])
+            if cid not in currency_ids:
+                self._logger.warn(f"Unknown currency-id {cid}")
+            defi_coins_ids.append(cid)
+
+        defi_coins_ids.sort()
+
+        with open(out_file, "w", newline="", encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["currency-id"])
+            for cid in defi_coins_ids:
+                writer.writerow([cid])
+
+
+if __name__ == "__main__":
+    try:
+        CMCDataGenerator("staging", "TeamIDCAdmin-staging").generate()
+        exit(0)
+    except OSError:
+        exit(1)
--- utils/external_data_generator/main.py
+++ utils/external_data_generator/main.py
@@ -5,15 +5,15 @@ import argparse
 import enum
 import os
 import shutil
-import tarfile
-import tempfile
 
 from adx import ADXDataGenerator
 from biva import BivaDataGenerator
 from canada import CanadaDataGenerator
 from cboe import CBOEDataGenerator
 from cftc_code import CFTCDataGenerator
+from cmc_defi import CMCDataGenerator
 from cme import CMEDataGenerator
+from corpacts import CorpactsDataGenerator
 from korea import KoreaDataGenerator
 from lang_and_shwarz import Lang, Schwarz
 from lib.ConsoleOutput import ConsoleOutput
@@ -22,15 +22,14 @@ from mstar import MstarDataGenerator
 from nasdaq_gids import NASDAQGIDSDataGenerator
 from nasdaqtrader import NASDAQTraderDataGenerator
 from nyse import NyseDataGenerator
+from otc import OtcDataGenerator
+from s3_utils import download_state, upload_state, compare_with_remote
 from saudi import SAUDIDataGenerator
 from shanghai import ShanghaiDataGenerator
 from taipei import TaipeiDataGenerator
 from tokyo import TokyoDataGenerator
 from twse import TwseDataGenerator
-from otc import OtcDataGenerator
-from corpacts import CorpactsDataGenerator
-from upload_to_bucket import run_s3_process_snapshot, run_storage_download, run_s3_upload
-from utils import git_commit
+from utils import archive_files
 
 
 class Codes(enum.IntEnum):
@@ -40,7 +39,68 @@ class Codes(enum.IntEnum):
 
 
 def main(args, logger):
-    data_clusters = {
+    bucket_name = get_environment()
+    cluster_name = args.data_cluster
+    data_cluster = get_clusters(args).get(cluster_name)
+    if not data_cluster:
+        logger.error(f"Unknown data cluster: {cluster_name}")
+        return Codes.ERROR
+
+    for handler in data_cluster['handlers']:
+        state_name = f"{handler.get('state', cluster_name)}.tar.gz"
+        state_dir = handler.get('state_dir', "external")
+
+        if args.copy:
+            remote_state_archive = download_state(f"{state_dir}/{state_name}")
+            logger.log(f"Uploading state {state_dir}/{state_name}... ", upload_state, remote_state_archive, bucket_name, f"{state_dir}/{state_name}")
+            logger.info(f"Successfully uploaded {state_name} to s3://{bucket_name}/{state_dir}/{state_name}.")
+            shutil.rmtree(remote_state_archive, ignore_errors=True)
+            continue
+
+        try:
+            files = handler['generator']()
+        except Exception as e:
+            logger.error(f"Failed to generate files for '{cluster_name}' data cluster CAUSED BY: {e}")
+            return Codes.ERROR
+
+        if args.compare:
+            remote_state_archive = download_state(f"{state_dir}/{state_name}")
+            diffs = logger.log(f"Comparing new files with files in archive {remote_state_archive}... ", compare_with_remote, files, remote_state_archive)
+            for d in diffs:
+                logger.info(d)
+            shutil.rmtree(remote_state_archive, ignore_errors=True)
+
+        try:
+            archive_name = logger.log("Archiving new files... ", archive_files, files, state_name)
+            logger.log(f"Uploading state {state_dir}/{state_name}... ", upload_state, archive_name, bucket_name, f"{state_dir}/{state_name}")
+            logger.info(f"Successfully uploaded {state_name} to s3://{bucket_name}/{state_dir}/{state_name}.")
+        except Exception as e:
+            logger.error(f"Failed to update {','.join(files)} files into '{state_dir}/{state_name}' state for '{cluster_name}' data cluster CAUSED BY:")
+            logger.error(e)
+            return Codes.ERROR
+
+        shutil.rmtree(archive_name, ignore_errors=True)
+
+    return Codes.OK
+
+
+def get_environment() -> str:
+    environment = os.environ.get('ENVIRONMENT', None)
+
+    if environment == "production":
+        bucket_name = 'tradingview-sourcedata-storage'
+    elif environment == "stable":
+        bucket_name = 'tradingview-sourcedata-storage-stable'
+    elif environment == "staging":
+        bucket_name = 'tradingview-sourcedata-storage-staging'
+    else:
+        raise EnvironmentError(f"Unknown ENVIRONMENT value: {environment}")
+
+    return bucket_name
+
+
+def get_clusters(args):
+    return {
         "adx": {"handlers": [{"generator": ADXDataGenerator().generate}]},
         "biva": {"handlers": [{"generator": BivaDataGenerator().generate}]},
         "canada": {"handlers": [{"generator": CanadaDataGenerator().generate, "state": "cse"}]},
@@ -65,72 +125,8 @@ def main(args, logger):
         "cboe": {"handlers": [{"generator": CBOEDataGenerator().generate}]},
         "otc": {"handlers": [{"generator": OtcDataGenerator(args.branch).generate}]},
         "corpacts": {"handlers": [{"generator": CorpactsDataGenerator().generate, "state_dir": "tvc"}]},
+        "cmc_defi": {"handlers": [{"generator": CMCDataGenerator(args.branch, "TeamIDCAdmin-staging").generate, "state_dir": "tvc", "state": "defi"}]},
     }
-    cluster_name = args.data_cluster
-    data_cluster = data_clusters.get(cluster_name)
-    if not data_cluster:
-        logger.error(f"Unknown data cluster: {cluster_name}")
-        return Codes.ERROR
-
-    if args.copy:
-        return download_and_upload(data_cluster, cluster_name, args.branch)
-
-    return generate_and_upload(data_cluster, cluster_name, args.branch)
-
-
-def download_and_upload(data_cluster, cluster_name, git_branch):
-    for handler in data_cluster['handlers']:
-        state_name = handler.get('state', cluster_name)
-        state_dir = handler.get('state_dir', "external")
-        with tempfile.NamedTemporaryFile(suffix=".tar.gz", delete=False) as tmp_file:
-            tmp_file.close()
-            try:
-                logger.info(f"Copying state {state_dir}/{state_name}")
-                run_storage_download(state_dir, state_name, tmp_file.name)
-                run_s3_upload(tmp_file.name, state_dir, state_name)
-
-                with tempfile.TemporaryDirectory() as tmp_dir:
-                    logger.info(f"Unpacking {tmp_file.name}")
-                    with tarfile.open(tmp_file.name, "r") as tar:
-                        tar.extractall(path=tmp_dir)
-                        tar.close()
-                    files = os.listdir(tmp_dir)
-                    for fn in files:
-                        logger.info(f"Copying {fn} from archive to working directory")
-                        shutil.copy(os.path.join(tmp_dir, fn), os.path.join(".", fn))
-
-                git_commit(files, git_branch)
-            except Exception as e:
-                logger.error(f"Failed to download and upload '{state_dir}/{state_name}' data CAUSED BY: {e}")
-                return Codes.ERROR
-
-    return Codes.OK
-
-
-def generate_and_upload(data_cluster, cluster_name, git_branch):
-    files_to_commit = []
-    for handler in data_cluster['handlers']:
-        try:
-            files = handler['generator']()
-        except Exception as e:
-            logger.error(f"Failed to generate files for '{cluster_name}' data cluster CAUSED BY: {e}")
-            return Codes.ERROR
-        files_to_commit.extend(files)
-        state_name = handler.get('state', cluster_name)
-        state_dir = handler.get('state_dir', "external")
-        try:
-            run_s3_process_snapshot(files, state_dir, state_name)
-        except Exception as e:
-            logger.error(f"Failed to update {','.join(files)} files into '{state_dir}/{state_name}' state for '{cluster_name}' data cluster CAUSED BY: {e}")
-            return Codes.ERROR
-
-    try:
-        git_commit(files_to_commit, git_branch)
-    except Exception as e:
-        logger.error(f"Failed to commit [{','.join(files_to_commit)}] files generated by '{cluster_name}' data cluster handlers CAUSED BY {e}")
-        return Codes.ERROR
-
-    return Codes.OK
 
 
 if __name__ == "__main__":
@@ -141,6 +137,7 @@ if __name__ == "__main__":
     parser.add_argument("--branch", type=str, default="", required=False,
                         help="Branch for delivery changed files (if empty then changed files will not delivers)")
     parser.add_argument("--copy", action=argparse.BooleanOptionalAction, help="Download from prod and upload")
+    parser.add_argument("--compare", action=argparse.BooleanOptionalAction, help="Compare with remote")
 
     try:
         exit(main(parser.parse_args(), logger))
