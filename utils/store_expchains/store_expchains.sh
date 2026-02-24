2c6364daf0ffd3f1f259741f9361d14584640d27
Igor Matsko <imatsko@tradingview.com>
2026-02-24 14:48:37 +0300

    IDC-5288: add nse_currency_futures expchains

--- utils/store_expchains/store_expchains.sh
+++ utils/store_expchains/store_expchains.sh
@@ -45,6 +45,7 @@ SI_OTHER_GROUPS=(
   "six_nymex_globex_b_futures"
   "six_nymex_globex_mini_futures"
   "six_istanbul_futures"
+  "nse_currency_futures"
   )
 for GROUP in "${SI_OTHER_GROUPS[@]}"; do
     echo "Getting $GROUP"
