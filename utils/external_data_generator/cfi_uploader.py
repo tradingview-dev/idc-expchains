583f789d00f1ef94ba985cee7d42a49aada858c1
Nikita Mishenin <nmishenin@tradingview.com>
2026-03-26 18:53:43 +0300

    IDC-5452: CFI to currencies

--- utils/external_data_generator/cfi_uploader.py
+++ utils/external_data_generator/cfi_uploader.py
@@ -67,13 +67,13 @@ class CFIUploader(DataGenerator):
 
 def upload_cfi(branch: str):
     for filename in os.listdir("cfi_dict"):
-        upload_state(f"cfi_dict/{filename}", get_bucket(branch), f"cfi/{filename}")
+        upload_state(f"cfi_dict/{filename}", get_bucket(branch), f"{filename}")
 
 
 def get_bucket(branch: str) -> str:
     buckets = {
-        "staging": "tradingview-pub-staging",
-        "master": "tradingview-pub"
+        "staging": "tradingview-currencies-staging",
+        "master": "tradingview-currencies"
     }
     return buckets[branch]
 
